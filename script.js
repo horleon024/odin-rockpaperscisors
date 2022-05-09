d9332bcfba7c15cd4fb9be72082fe552e9a039a7
horleon024 <horeftosleon@gmail.com>
2022-05-09 18:46:21 +0200

    Implement basic logic

--- script.js
+++ script.js
@@ -3,4 +3,33 @@ function computerPlay() {
 	return randomNumber % 3 == 0 ? "Rock" : (randomNumber % 3 == 1 ? "Paper" : "Scissors");
 }
 
-console.log(computerPlay());
+function playRound(playerSelection, computerSelection) {
+	if (playerSelection.toLowerCase() === "rock") {
+		if (computerSelection === "Scissors") {
+			return "You win! Rock beats Scissors";
+		} else if (computerSelection === "Paper") {
+			return "You lose! Paper beats Rock";
+		} else {
+			return "It's a draw";
+		}
+	} else if (playerSelection.toLowerCase() === "scissors") {
+		if (computerSelection === "Rock") {
+			return "You lose! Rock beats Scissors";
+		} else if (computerSelection === "Paper") {
+			return "You win! Scissors beats Paper";
+		} else {
+			return "It's a draw";
+		}
+	} else {
+		if (computerSelection === "Rock") {
+			return "You win! Paper beats Rock";
+		} else if (computerSelection === "Scissors") {
+			return "You lose! Scissors beats Paper";
+		} else {
+			return "It's a draw";
+		}
+	}
+}
+
+const playerSelection = "rock";
+console.log(playRound(playerSelection, computerPlay()));
