885d2d84af443de6b0cb32c17e91ae132c5249d2
horleon024 <horeftosleon@gmail.com>
2022-05-19 15:30:45 +0200

    implement game logic

--- script.js
+++ script.js
@@ -1,47 +1,71 @@
+const container = document.querySelector(".game");
+const buttons = document.querySelectorAll("button");
+const playerScore = document.querySelector(".playerScore");
+const computerScore = document.querySelector(".computerScore");
+const roundResult = document.querySelector(".roundResult");
+const finalResult = document.querySelector(".finalResult");
+const restartButton = document.createElement("button");
+
+restartButton.textContent = "Restart game";
+
 function	computerPlay() {
 	const randomNumber = Math.floor(Math.random() * 3);
 	return randomNumber % 3 == 0 ? "Rock" : (randomNumber % 3 == 1 ? "Paper" : "Scissors");
 }
 
+function restartGame(event) {
+	playerScore.textContent = 0;
+	computerScore.textContent = 0;
+	roundResult.textContent = "";
+	finalResult.textContent = "";
+	container.removeChild(restartButton);
+}
+
+function	checkForWinner(scoreAfterRound, isPlayerScore) {
+	if (scoreAfterRound < 5) return;
+	if (isPlayerScore) finalResult.textContent = "You won the game";
+	else finalResult.textContent = "You lost the game";
+
+	container.appendChild(restartButton);
+	restartButton.addEventListener('click', restartGame);	
+}
+
 function	playRound(event) {
 	const playerSelection = event.target.textContent;
 	const computerSelection = computerPlay();
-	const roundDiv = document.querySelector(".currentRound");
-	const playerScore = document.querySelector(".playerScore");
-	const computerScore = document.querySelector(".computerScore");
 	let isPlayerWin = true;
 	let isDraw = false;
 	if (playerSelection === "Rock") {
 		if (computerSelection === "Scissors") {
-			roundDiv.textContent = "You win this round! Rock beats Scissors";
+			roundResult.textContent = "You win this round! Rock beats Scissors";
 			isPlayerWin = true;
 		} else if (computerSelection === "Paper") {
-			roundDiv.textContent = "You lose this round! Paper beats Rock";
+			roundResult.textContent = "You lose this round! Paper beats Rock";
 			isPlayerWin = false;
 		} else {
-			roundDiv.textContent = "This round is a draw";
+			roundResult.textContent = "This round is a draw";
 			isDraw = true;
 		}
 	} else if (playerSelection === "Scissors") {
 		if (computerSelection === "Rock") {
-			roundDiv.textContent = "You lose this round! Rock beats Scissors";
+			roundResult.textContent = "You lose this round! Rock beats Scissors";
 			isPlayerWin = false;
 		} else if (computerSelection === "Paper") {
-			roundDiv.textContent = "You win this round! Scissors beats Paper";
+			roundResult.textContent = "You win this round! Scissors beats Paper";
 			isPlayerWin = true;
 		} else {
-			roundDiv.textContent = "This round is a draw";
+			roundResult.textContent = "This round is a draw";
 			isDraw = true;
 		}
 	} else {
 		if (computerSelection === "Rock") {
-			roundDiv.textContent = "You win this round! Paper beats Rock";
+			roundResult.textContent = "You win this round! Paper beats Rock";
 			isPlayerWin = true;
 		} else if (computerSelection === "Scissors") {
-			roundDiv.textContent = "You lose this round! Scissors beats Paper";
+			roundResult.textContent = "You lose this round! Scissors beats Paper";
 			isPlayerWin = false;
 		} else {
-			roundDiv.textContent = "This round is a draw";
+			roundResult.textContent = "This round is a draw";
 			isDraw = true;
 		}
 	}
@@ -49,27 +73,15 @@ function	playRound(event) {
 		let scoreAfterRound = parseInt(playerScore.textContent);
 		scoreAfterRound++;
 		playerScore.textContent = scoreAfterRound;
+		checkForWinner(scoreAfterRound, true);
 	}
 	else if (!isDraw && !isPlayerWin) {
 		let scoreAfterRound = parseInt(computerScore.textContent);
 		scoreAfterRound++;
 		computerScore.textContent = scoreAfterRound;
+		checkForWinner(scoreAfterRound, false);
 	}
 }
 
-/*function	game() {
-	let	playerScore = 0;
-	let	computerScore = 0;
-
-	for (let i = 0; i < 5; i++) {
-		const result = playRound(prompt("Your choice: "), computerPlay());
-		result == 1 ? playerScore++ : (result == 2 ? computerScore++ : 0);
-	}
-	console.log(playerScore > computerScore ? "You won the game" : (playerScore == computerScore ? "It's a draw" : "You lost the game"));
-}*/
-
-const buttons = document.querySelectorAll("button");
-
 buttons.forEach(button => button.addEventListener('click', playRound));
 
-//game();
